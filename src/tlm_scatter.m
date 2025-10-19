5cbbc93f12f307b9ecaa010d2fb61af0666eee00
Adrian Niazi <adrian.izi@protonmail.com>
2025-10-19 21:52:25 +0200

    feat: add scattering function

--- src/tlm_scatter.m
+++ src/tlm_scatter.m
@@ -1,16 +1,17 @@
-function [outputArg1,outputArg2] = tlm_scatter(inputArg1,inputArg2)
-%TLM_SCATTER Summary of this function goes here
-%   Detailed explanation goes here
-arguments (Input)
-    inputArg1
-    inputArg2
-end
+function b_next = tlm_scatter(a)
+%TLM_SCATTER scattering at timestep k+1 as a result of pressures at
+%timestep k
+%  Can calculate b as a result of scattering matrix S and vector a for each
+%  node
+    [Nx, Ny, channels] = size(a);
+    b_next = zeros(Nx,Ny,channels); 
+    S = params.S;
 
-arguments (Output)
-    outputArg1
-    outputArg2
-end
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+    for i = 1:Nx
+        for j = 1:Ny
+            a_node = squeeze(a(i,j,:)); %extract 4x1 column vector of incident pressures
+            b_node = 0.5 * S * a_node;  %applies scattering
+            b_next(i,j,:) = b_node;     %Puts them in the total b_next
+        end
+    end
 end
